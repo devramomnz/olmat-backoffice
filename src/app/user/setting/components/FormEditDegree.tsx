fec83906d12ff2b74477552d7eea3793d57a4ccd
mchramaa <rama28moch@gmail.com>
2024-05-07 15:15:20 +0700

    update: open disable degree edit

--- src/app/user/setting/components/FormEditDegree.tsx
+++ src/app/user/setting/components/FormEditDegree.tsx
@@ -23,7 +23,6 @@ export default function FormEditDegree(props: IProps) {
           labelName="Id Jenjang"
         />
         <AntInput
-          disabled
           onChange={handleChange}
           name="name"
           labelName="Nama Jenjang"
